e73831a114e5f6a27caac1acdde377448d4cbc58
SergeyGitAccount <l_w_d@i.ua>
2024-08-04 19:44:11 +0300

    initial commit

--- tests/interactionWithWebElements/locatorSyntaxRules.spec.ts
+++ tests/interactionWithWebElements/locatorSyntaxRules.spec.ts
@@ -9,16 +9,22 @@ test.beforeEach("start beforeEach test", async ({ page }) => {
 test("LocatorSyntaxRules", async ({ page }) => {
   //by tag name
   page.locator('input');
+  page.locator('span');
   //by id
   await page.locator('#inputEmail1').first().click();
+  await page.locator('#inputPassword2');
    //by class
    page.locator('.shape-rectangle');
+   page.locator('.input-full-width')
     //by attributeS
    page.locator('[placeholder="Email"]')
+   page.locator('[placeholder="Password"]')
     //by class value (full)
     page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+    page.locator('[class="native-input visually-hidden"]')
     //combine different slectors
     page.locator('input[placeholder="Email"].shape-rectangle')
+    page.locator('input[type="radio"].native-input visually-hidden')
      //by Xpath (NOT RECOMENDED)
     page.locator('//*[@id="inputEmail1"]')
      //by partial text match
--- tests/interactionWithWebElements/userFacingLocators.spec.ts
+++ tests/interactionWithWebElements/userFacingLocators.spec.ts
@@ -7,12 +7,17 @@ test.beforeEach("start beforeEach test", async ({ page }) => {
 });
 
 test("UserFacingLocators.spec", async ({ page }) => {
-  //by tag name
-  await page.getByRole('textbox', { name: 'Email' }).first().click();
-  await page.getByRole('button', { name: 'Sign in' }).first().click();
-  await page.getByLabel('Email').first().click();
-  await page.getByPlaceholder('Jane Doe').click();
-  await page.getByText('Using the Grid').click();
-  await page.getByTitle('IoT Dashboard').click();
-  
+  await page.getByRole("textbox", { name: "Email" }).first().click();
+  await page.getByRole("textbox", { name: "Jane Doe" }).click();
+  await page.getByRole("button", { name: "Sign in" }).first().click();
+  await page.getByRole("button", { name: "Submit" }).first().click();
+  await page.getByLabel("Email").first().click();
+  await page.getByLabel("Password").first().click();
+  await page.getByLabel("Password").nth(1).click();
+  await page.getByPlaceholder("Jane Doe").click();
+  await page.getByPlaceholder("Recipients").click();
+  await page.getByText("Using the Grid").click();
+  await page.getByText("Inline form").click();
+  await page.getByTitle("IoT Dashboard").click();
+  await page.getByTitle("Forms").click();
 });
