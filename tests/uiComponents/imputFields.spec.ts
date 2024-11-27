89dff4552f0958dc1bd0599ad3e7ebe132da02d6
SergeyGitAccount <l_w_d@i.ua>
2024-11-27 23:37:38 +0200

    initial commit

--- tests/uiComponents/imputFields.spec.ts
+++ tests/uiComponents/imputFields.spec.ts
@@ -27,3 +27,78 @@ test.describe("Form Layouts page", () => {
     await expect(usingTheGridEmailInput).toHaveValue("test2@test.com");
   });
 });
+
+
+test.describe("Form Layouts page two", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Forms").click();
+    await page.getByText("Form Layouts").click();
+  });
+  test("Input fields two", async ({ page }) => {
+    const basicFormEmailInput = page
+      .locator("nb-card", { hasText: "Basic form" })
+      .getByRole("textbox", { name: "Email" });
+
+    await basicFormEmailInput.fill("test@test.com");
+    await basicFormEmailInput.clear();
+    await basicFormEmailInput.pressSequentially("test2@test.com", {
+      delay:10,
+    });
+
+    //generic assertion
+    const inputValue = await basicFormEmailInput.inputValue();
+    expect(inputValue).toEqual("test2@test.com");
+    //locator assertion
+    await expect(basicFormEmailInput).toHaveValue("test2@test.com");
+  });
+});
+
+
+test.describe("Form Layouts page three", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Forms").click();
+    await page.getByText("Form Layouts").click();
+  });
+  test("Input fields three", async ({ page }) => {
+    const horizontalFormPasswordInput = page
+      .locator("nb-card", { hasText: "Horizontal form" })
+      .getByRole("textbox", { name: "Password" });
+
+    await horizontalFormPasswordInput.fill("test@test.com");
+    await horizontalFormPasswordInput.clear();
+    await horizontalFormPasswordInput.pressSequentially("test2@test.com", {
+      delay:10,
+    });
+
+    //generic assertion
+    const inputValue = await horizontalFormPasswordInput.inputValue();
+    expect(inputValue).toEqual("test2@test.com");
+    //locator assertion
+    await expect(horizontalFormPasswordInput).toHaveValue("test2@test.com");
+  });
+});
+
+
+test.describe("Form Layouts page four", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Forms").click();
+    await page.getByText("Form Layouts").click();
+  });
+  test("Input fields four", async ({ page }) => {
+    const horizontalFormPasswordInput = page
+      .locator("nb-card", { hasText: "Horizontal form" })
+      .getByRole("textbox", { name: "Password" });
+
+    await horizontalFormPasswordInput.fill("test@test.com");
+    await horizontalFormPasswordInput.clear();
+    await horizontalFormPasswordInput.pressSequentially("test2@test.com", {
+      delay:10,
+    });
+
+    //generic assertion
+    const inputValue = await horizontalFormPasswordInput.inputValue();
+    expect(inputValue).toEqual("test2@test.com");
+    //locator assertion
+    await expect(horizontalFormPasswordInput).toHaveValue("test2@test.com");
+  });
+});
